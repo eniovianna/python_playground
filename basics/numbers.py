3ed0b3b5a6ca160900eeeb3eb2561962d9b8c91c
Ênio Viana <eniocc@gmail.com>
2020-06-20 18:43:33 -0300

    Initial commit

---
+++ basics/numbers.py
@@ -0,0 +1,25 @@
+'''
+Filename: c:\python_playground\basics\numbers.py
+Path: c:\python_playground\basics
+Created Date: Saturday, June 20th 2020, 6:32:14 pm
+Author: Ênio Viana
+
+Copyright (c) 2020 Your Company
+'''
+# Types: int, float, complex
+
+# A simple integer number
+a = 496
+print(type(a))
+
+#  Classical PI number
+pi = 3.14159265359 
+print(type(pi))
+
+# A complex resistance
+Z = 2 - 0.31j 
+print(type(Z))
+print(Z.real, ' real part')
+print(Z.imag, ' imaginary part')
+print(type(Z.real))
+print(type(Z.imag))
---
+++ basics/strings.py
@@ -0,0 +1,39 @@
+'''
+Filename: c:\python_playground\basics\strings.py
+Path: c:\python_playground\basics
+Created Date: Saturday, June 20th 2020, 6:09:04 pm
+Author: Ênio Viana
+
+Copyright (c) 2020 Your Company
+'''
+# A simple message with double quotes
+message = "Hello world!"
+print(message)
+
+# Another simple message with single quotes
+message2 = 'Hello world 2!'
+print(message2)
+
+# A message with error
+#message3 = 'I'm new in Python' 
+#print(message3)
+
+# Way 1 to fix it
+message4 = 'I\'m new in Python' 
+print(message4)
+
+# Way 2 to fix it
+message5 = "I'm new in Python"
+print(message5)
+
+# Another message with error
+#message6 = "The citation: "bla blaa" generates an error"
+#print(message6)
+
+message7 = 'The citation: "bla blaa" generates an error'
+print(message7)
+
+# A complex string
+message8 = """This message is complicated. I'm making a strong effort to write ir 100% correct!"""
+print(message8)
+
